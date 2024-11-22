a89622de9e31ae110cfb5f7290497def41642bb9
VITOR RODRIGUES DE LIMA <vitorrodriguesdelima49@gmail.com>
2024-11-22 12:05:23 -0300

    teste
    teste

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-1490.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-c3738ddf' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-8bb4b826' );
-});
- });
