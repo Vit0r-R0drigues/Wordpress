3b7f3cc8297162b6e367246094dea4c81accf037
VITOR RODRIGUES DE LIMA <vitorrodriguesdelima49@gmail.com>
2024-12-02 10:47:14 -0300

    atu
    atu

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-1490.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1490.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-c3738ddf' );
-});
-window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-8bb4b826' );
 });
  });
